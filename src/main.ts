52f6d39401279fdd78dc1c79f460824e61bbce48
eexy <eexdhe@gmail.com>
2025-12-10 20:04:20 +0100

    refactor: rename view

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,13 @@ import "./index.css"
 import App from "./App.vue";
 import { createPinia } from "pinia"
 import { createRouter, createWebHistory } from "vue-router";
-import Game from "./views/games/Game.vue";
+import Games from "./views/games/Game.vue";
 
 const pinia = createPinia()
 const router = createRouter({
     history: createWebHistory(), routes: [
         { path: '/', redirect: '/games' },
-        { path: '/games', component: Game }
+        { path: '/games', component: Games }
     ]
 });
 
