df657fd92cf81558c81c8c74f4823565782f3870
eexy <eexdhe@gmail.com>
2025-12-10 20:02:12 +0100

    feat: install vue-router and create games view

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,9 @@
 <template>
-    <SidebarProvider>
-        <AppSidebar></AppSidebar>
-        <main class="">
-            <SidebarTrigger></SidebarTrigger>
-            <Button @click="console.log('hello world')">Hello world!</Button>
-        </main>
-    </SidebarProvider>
+    <RouterView></RouterView>
 </template>
 
 <script setup lang="ts">
-import { Button } from "@/components/ui/button"
-import { SidebarTrigger, SidebarProvider } from "@/components/ui/sidebar"
-import { onMounted } from "vue";
-import { invoke } from "@tauri-apps/api/core"
-import { Game } from "@/types/game";
-import { useGameStore } from "@/stores/game.store";
-import { storeToRefs } from "pinia";
-import AppSidebar from "@/components/app-sidebar/AppSidebar.vue"
-
-const { games } = storeToRefs(useGameStore())
-
-onMounted(async () => {
-    const res: Game[] = await invoke("get_games")
-    games.value = res
-})
+import { RouterView } from 'vue-router';
 </script>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,15 @@ import { createApp } from "vue";
 import "./index.css"
 import App from "./App.vue";
 import { createPinia } from "pinia"
+import { createRouter, createWebHistory } from "vue-router";
+import Game from "./views/games/Game.vue";
 
 const pinia = createPinia()
-createApp(App).use(pinia).mount("#app");
+const router = createRouter({
+    history: createWebHistory(), routes: [
+        { path: '/', redirect: '/games' },
+        { path: '/games', component: Game }
+    ]
+});
+
+createApp(App).use(pinia).use(router).mount("#app");
