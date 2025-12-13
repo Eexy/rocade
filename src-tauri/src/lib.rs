eda671d962aa7546a00a0f5790df0fe3780c2784
eexy <eexdhe@gmail.com>
2025-12-13 15:08:37 +0100

    feat: create twitch api client with function to get access token

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,12 +1,13 @@
 use core::panic;
 use std::collections::HashMap;
 
-use tauri::Manager;
+use tauri::{async_runtime::Mutex, Manager};
 
-use crate::steam::{SteamApiClient, SteamState};
+use crate::{steam::{SteamApiClient, SteamState}, twitch::TwitchApiClient};
 
 mod dotenv;
 mod steam;
+mod twitch;
 mod game;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -46,6 +47,23 @@ pub fn run() {
                 }
             }
 
+
+            match (app_config.get("TWITCH_CLIENT_ID"), app_config.get("TWITCH_CLIENT_SECRET")) {
+                (Some(client_id), Some(client_secret)) => {
+                    let handle = app.handle();
+                    tauri::async_runtime::block_on(async move {
+                        let mut twitch_api_client = TwitchApiClient::new(client_id.clone(), client_secret.clone());
+                        twitch_api_client.auth().await.expect("unable to get twitch access token");
+                        handle.manage::<TwitchApiClient>(twitch_api_client);
+                    });
+                },
+                _ => {
+                    panic!("Unable to load twitch config. Missing TWITCH_CLIENT_ID or TWITCH_CLIENT_SECRET in dotenv file")
+                }
+            }
+
+            
+
             
             #[cfg(debug_assertions)]
             {
@@ -55,6 +73,8 @@ pub fn run() {
                 dbg!(steam_state);
                 let steam_api_client = app.state::<SteamApiClient>();
                 dbg!(steam_api_client);
+                let twitch_api_client = app.state::<TwitchApiClient>();
+                dbg!(&twitch_api_client);
             }
 
             Ok(())
---
+++ src-tauri/src/twitch.rs
@@ -0,0 +1,50 @@
+use serde::Deserialize;
+use tauri_plugin_http::reqwest::Client;
+
+#[derive(Debug)]
+pub struct TwitchApiClient {
+    client_id: String,
+    client_secret: String,
+    access_token: Option<String>,
+    refresh_token: Option<String>,
+    client: Client,
+}
+
+#[derive(Deserialize)]
+pub struct TwitchAuthResponse {
+    access_token: String,
+}
+
+impl TwitchApiClient {
+    pub fn new(client_id: String, client_secret: String) -> Self {
+        return TwitchApiClient {
+            client_id,
+            client_secret,
+            access_token: None,
+            refresh_token: None,
+            client: tauri_plugin_http::reqwest::Client::new(),
+        };
+    }
+
+    pub async fn auth(&mut self) -> Result<(), String> {
+        let url = format!("https://id.twitch.tv/oauth2/token?client_id={}&client_secret={}&grant_type=client_credentials", self.client_id, self.client_secret);
+        let res = self
+            .client
+            .post(url)
+            .send()
+            .await
+            .map_err(|e| e.to_string())?;
+
+        let body = res.text().await.map_err(|e| e.to_string())?;
+
+        let parsed: TwitchAuthResponse = serde_json::from_str(&body).map_err(|e| e.to_string())?;
+
+        self.access_token = Some(parsed.access_token);
+
+        Ok(())
+    }
+
+    pub fn get_access_token(&self) -> Option<String> {
+        self.access_token.clone()
+    }
+}
