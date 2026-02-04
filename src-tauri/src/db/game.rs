18dc77ef88b15a0020021aa0cb4db3d165ae386e
eexy <eexdhe@gmail.com>
2026-02-04 19:21:06 +0100

    chore: remove unused method in game repository

--- src-tauri/src/db/game.rs
+++ src-tauri/src/db/game.rs
@@ -11,16 +11,6 @@ pub struct GameRow {
 pub struct GameRepository {}
 
 impl GameRepository {
-    pub async fn delete_games(pool: &Pool<Sqlite>) -> Result<(), sqlx::Error> {
-        let mut conn = pool.acquire().await?;
-
-        sqlx::query!("delete from games")
-            .execute(&mut *conn)
-            .await?;
-
-        Ok(())
-    }
-
     pub async fn get_games(pool: &Pool<Sqlite>) -> Result<Vec<GameRow>, sqlx::Error> {
         let mut conn = pool.acquire().await?;
 
@@ -50,15 +40,5 @@ impl GameRepository {
         .last_insert_rowid();
 
         Ok(id)
-    }
-
-    pub async fn get_game_by_id(pool: &Pool<Sqlite>, game_id: i64) -> Result<GameRow, sqlx::Error> {
-        let mut conn = pool.acquire().await?;
-
-        let game = sqlx::query_as!(GameRow, r#"select * from games where id = ?"#, game_id)
-            .fetch_one(&mut *conn)
-            .await?;
-
-        Ok(game)
     }
 }
