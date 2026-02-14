68cfb7f27a6edc0427f49a476e8fd661543554e0
eexy <eexdhe@gmail.com>
2026-02-14 17:26:33 +0100

    refactor: create function to build game sql query instead of having duplicate

--- src-tauri/src/db/game.rs
+++ src-tauri/src/db/game.rs
@@ -1,3 +1,5 @@
+use std::fmt::format;
+
 use serde::Serialize;
 use sqlx::{sqlite::SqliteRow, Pool, Row, Sqlite};
 
@@ -21,66 +23,82 @@ pub struct GameRepository {}
 
 impl GameRepository {
     pub async fn get_games(pool: &Pool<Sqlite>) -> Result<Vec<Game>, sqlx::Error> {
-        let games = sqlx::query(
-            "
-select 
-    games.id as id, 
-    games.name as name, 
-    games_store.store_id as store_id, 
-    summary, release_date, 
-    json_group_array(distinct genres.name) as genres, 
-    json_group_array(distinct companies.name) as studios, 
-    json_group_array(distinct artworks.artwork_id) as artworks, 
-    json_group_array(distinct covers.cover_id) as covers
-from games
-inner join developed_by on games.id = developed_by.game_id
-inner join companies on developed_by.studio_id = companies.id
-inner join belongs_to on games.id = belongs_to.game_id
-inner join genres on belongs_to.genre_id = genres.id
-inner join artworks on artworks.game_id = games.id
-inner join covers on covers.game_id = games.id
-inner join games_store on games_store.game_id = games.id
-group by games.id, games.name, games_store.store_id, games.summary, games.release_date
-order by games.name
-    ",
-        )
-        .map(|row: SqliteRow| {
-            let genres_json: Option<String> = row.get("genres");
-            let studios_json: Option<String> = row.get("studios");
-            let artworks_json: Option<String> = row.get("artworks");
-            let covers_json: Option<String> = row.get("covers");
+        let query = Self::build_query_string(None);
+        let games = sqlx::query(&query)
+            .map(|row: SqliteRow| {
+                let genres_json: Option<String> = row.get("genres");
+                let studios_json: Option<String> = row.get("studios");
+                let artworks_json: Option<String> = row.get("artworks");
+                let covers_json: Option<String> = row.get("covers");
 
-            Game {
-                id: row.get("id"),
-                release_date: row.get("release_date"),
-                name: row.get("name"),
-                developers: studios_json
-                    .as_ref()
-                    .and_then(|s| serde_json::from_str::<Vec<String>>(s).ok()),
-                genres: genres_json
-                    .as_ref()
-                    .and_then(|s| serde_json::from_str::<Vec<String>>(s).ok()),
-                is_installed: None,
-                summary: row.get("summary"),
-                artworks: artworks_json
-                    .as_ref()
-                    .and_then(|s| serde_json::from_str::<Vec<String>>(s).ok()),
-                cover: covers_json
-                    .as_ref()
-                    .and_then(|s| serde_json::from_str::<Vec<String>>(s).ok())
-                    .and_then(|mut v| v.pop()),
-                store_id: row.get("store_id"),
-            }
-        })
-        .fetch_all(pool)
-        .await?;
+                Game {
+                    id: row.get("id"),
+                    release_date: row.get("release_date"),
+                    name: row.get("name"),
+                    developers: studios_json
+                        .as_ref()
+                        .and_then(|s| serde_json::from_str::<Vec<String>>(s).ok()),
+                    genres: genres_json
+                        .as_ref()
+                        .and_then(|s| serde_json::from_str::<Vec<String>>(s).ok()),
+                    is_installed: None,
+                    summary: row.get("summary"),
+                    artworks: artworks_json
+                        .as_ref()
+                        .and_then(|s| serde_json::from_str::<Vec<String>>(s).ok()),
+                    cover: covers_json
+                        .as_ref()
+                        .and_then(|s| serde_json::from_str::<Vec<String>>(s).ok())
+                        .and_then(|mut v| v.pop()),
+                    store_id: row.get("store_id"),
+                }
+            })
+            .fetch_all(pool)
+            .await?;
 
         Ok(games)
     }
 
     pub async fn get_game_by_id(pool: &Pool<Sqlite>, game_id: i64) -> Result<Game, sqlx::Error> {
-        let game = sqlx::query(
-            "
+        let query = Self::build_query_string(Some(game_id));
+        let game = sqlx::query(&query)
+            .bind(game_id)
+            .map(|row: SqliteRow| {
+                let genres_json: Option<String> = row.get("genres");
+                let studios_json: Option<String> = row.get("studios");
+                let artworks_json: Option<String> = row.get("artworks");
+                let covers_json: Option<String> = row.get("covers");
+
+                Game {
+                    id: row.get("id"),
+                    release_date: row.get("release_date"),
+                    name: row.get("name"),
+                    developers: studios_json
+                        .as_ref()
+                        .and_then(|s| serde_json::from_str::<Vec<String>>(s).ok()),
+                    genres: genres_json
+                        .as_ref()
+                        .and_then(|s| serde_json::from_str::<Vec<String>>(s).ok()),
+                    is_installed: None,
+                    summary: row.get("summary"),
+                    artworks: artworks_json
+                        .as_ref()
+                        .and_then(|s| serde_json::from_str::<Vec<String>>(s).ok()),
+                    cover: covers_json
+                        .as_ref()
+                        .and_then(|s| serde_json::from_str::<Vec<String>>(s).ok())
+                        .and_then(|mut v| v.pop()),
+                    store_id: row.get("store_id"),
+                }
+            })
+            .fetch_one(pool)
+            .await?;
+
+        Ok(game)
+    }
+
+    fn build_query_string(game_id: Option<i64>) -> String {
+        let base_query = "
 select 
     games.id as id, 
     games.name as name, 
@@ -98,44 +116,19 @@ inner join genres on belongs_to.genre_id = genres.id
 inner join artworks on artworks.game_id = games.id
 inner join covers on covers.game_id = games.id
 inner join games_store on games_store.game_id = games.id
-where games.id = ?1
+";
+
+        let order_clause = "
 group by games.id, games.name, games_store.store_id, games.summary, games.release_date
 order by games.name
-    ",
-        )
-        .bind(game_id)
-        .map(|row: SqliteRow| {
-            let genres_json: Option<String> = row.get("genres");
-            let studios_json: Option<String> = row.get("studios");
-            let artworks_json: Option<String> = row.get("artworks");
-            let covers_json: Option<String> = row.get("covers");
+";
+        let where_clause = if game_id.is_some() {
+            " where games.id =  ?"
+        } else {
+            ""
+        };
 
-            Game {
-                id: row.get("id"),
-                release_date: row.get("release_date"),
-                name: row.get("name"),
-                developers: studios_json
-                    .as_ref()
-                    .and_then(|s| serde_json::from_str::<Vec<String>>(s).ok()),
-                genres: genres_json
-                    .as_ref()
-                    .and_then(|s| serde_json::from_str::<Vec<String>>(s).ok()),
-                is_installed: None,
-                summary: row.get("summary"),
-                artworks: artworks_json
-                    .as_ref()
-                    .and_then(|s| serde_json::from_str::<Vec<String>>(s).ok()),
-                cover: covers_json
-                    .as_ref()
-                    .and_then(|s| serde_json::from_str::<Vec<String>>(s).ok())
-                    .and_then(|mut v| v.pop()),
-                store_id: row.get("store_id"),
-            }
-        })
-        .fetch_one(pool)
-        .await?;
-
-        Ok(game)
+        format!("{}{}{}", base_query, where_clause, order_clause)
     }
 
     pub async fn get_game_store_id(
